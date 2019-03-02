3311ec267b315720e51cc8125b60dbbf4e211ba1
deka108 <deka108@gmail.com>
2019-03-02 16:35:24 +0700

    Update config

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,7 +7,7 @@ bold=`tput bold`
 
 replace_dotenvfile(){
     sed -ie "s@PROJECT_HOME=.*@PROJECT_HOME=$(pwd)@" "${1}"
-    sed -ie "s@APP_CONFIG=.*@APP_CONFIG=$(pwd)/config/app/{{ cookiecutter.app_name }}_config.yaml@" "${1}"
+    sed -ie "s@PROJECT_CONFIG=.*@PROJECT_CONFIG=$(pwd)/config/config.yaml@" "${1}"
     rm "${1}e"
 }
 
